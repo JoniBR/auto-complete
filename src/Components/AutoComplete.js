f7430c1a896b101542dcd3d9741644803193c30b
Yoni Braslaver <Yoni@zira.us>
2020-05-18 17:36:59 +0300

    error handling

--- src/Components/AutoComplete.js
+++ src/Components/AutoComplete.js
@@ -24,8 +24,13 @@ class AutoComplete extends Component {
      searchTerm = searchTerm.toLowerCase()
      this.setState({ searchTerm , showMenu:true})
     if(searchTerm){
+      try{
       const filteredPokemonList = await this.getPokemons(searchTerm)
-      this.setState({pokemonList:filteredPokemonList})
+        this.setState({ pokemonList: filteredPokemonList })
+      } catch(error){
+        console.error(error)
+        this.setState({error:true})
+      }
     }
   }
 
@@ -52,24 +57,39 @@ class AutoComplete extends Component {
    * loads pokemon list from mock API call
    */
   async componentDidMount(){
+    try{
     const pokemonList = await this.getPokemons()
     this.setState({pokemonList})
+    } catch(error){
+      console.error(error)
+      this.setState({error:true})
+    }
   }
-
+  renderOptionsMenu = (error, pokemonList, searchTerm, showMenu) =>{
+    if(error){
+      return (<h1> Sorry, An error has occurred. Please try to refresh the page.</h1>)
+    } else{
+      return (
+      <React.Fragment>
+          {searchTerm && showMenu &&
+        (pokemonList.length ?
+          <div className='menu'>
+            {pokemonList.map((pokemon, i) => <ListItem text={pokemon} searchTerm={searchTerm} key={i} handleItemClick={this.handleItemClick} />)}
+          </div>
+          : <div className='no-results'>No Results...</div>)
+      }
+        { !showMenu ? `You've selected ${searchTerm}!` : ''}
+        </React.Fragment>)
+  }}
   
   render() {
+    const { error, pokemonList, searchTerm, showMenu } = this.state
     return (
       <div className='main'>
       <form>
-          <input type="search" className='input' placeholder='Search Pokemon' autoFocus onChange={this.handleInputChange} value={this.state.searchTerm}/>
+          <input type="search" className='input' placeholder='Search Pokemon' autoFocus onChange={this.handleInputChange} value={searchTerm}/>
         </form>
-        {this.state.searchTerm && this.state.showMenu &&
-          (this.state.pokemonList.length ? 
-          <div className='menu'> 
-          {this.state.pokemonList.map((pokemon, i) => <ListItem text={pokemon} searchTerm={this.state.searchTerm} key={i} handleItemClick={this.handleItemClick}/>)}
-          </div>
-          : <div className='no-results'>No Results...</div>)}
-        {!this.state.showMenu ?  `You've selected ${this.state.searchTerm}!` : ''}
+        {this.renderOptionsMenu(error, pokemonList, searchTerm, showMenu)}
       </div>
     );
   }
